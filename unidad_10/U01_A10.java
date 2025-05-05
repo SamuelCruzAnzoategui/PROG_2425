acf754d909faa23d7d0b2b1a6dc58aa132e2d01a
Samuel Cruz Anzoategui <samycru3106@gmail.com>
2025-05-05 10:59:35 +0200

    U01 modificado

--- unidad_10/U01_A10.java
+++ unidad_10/U01_A10.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class U01_A10 {
 
     public static void main(String[] args) {
-        try {
-            leerEntero();
-        } catch (Exception e) {
-            System.out.println(e);
+        boolean valido = false;
+        while (valido == false) {
+            valido = true;
+            try {
+                leerEntero();
+            } catch (Exception e) {
+                System.out.println(e);
+                valido = false;
+            }
         }
     }
 
-    static Integer leerEntero() {
+    static Integer leerEntero() throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduce un número entero: ");
         Integer num = sc.nextInt();
